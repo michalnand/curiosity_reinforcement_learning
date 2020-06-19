d4657c405c0f5fd96cec3c072d978287395ea466
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-06-19 22:40:33 +0200

    results

--- src/models/atari_ppo/src/config.py
+++ src/models/atari_ppo/src/config.py
@@ -3,12 +3,12 @@ class Config():
     def __init__(self):        
         self.gamma          = 0.99
 
-        self.update_iterations  = 128
+        self.update_iterations  = 256
         self.eps_clip           = 0.2
         self.entropy_beta       = 0.01
         self.training_epochs    = 4
 
-        self.learning_rate  = 0.0005
+        self.learning_rate  = 0.00025
         
 
 
