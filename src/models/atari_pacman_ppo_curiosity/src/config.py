7a98b1186e5106de8e5ef207fbbd27bc421396d6
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-06-20 09:29:26 +0200

    results

--- src/models/atari_pacman_ppo_curiosity/src/config.py
+++ src/models/atari_pacman_ppo_curiosity/src/config.py
@@ -7,8 +7,8 @@ class Config():
         self.eps_clip           = 0.2
         self.entropy_beta       = 0.01
         self.training_epochs    = 4 
-
-        self.learning_rate  = 0.0001
+ 
+        self.learning_rate  = 0.00025
         
 
 
