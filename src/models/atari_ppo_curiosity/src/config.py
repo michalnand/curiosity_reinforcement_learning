81112b1ea7609f4875ad2af0430e1500b228d101
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-06-20 17:12:15 +0200

    results

--- src/models/atari_ppo_curiosity/src/config.py
+++ src/models/atari_ppo_curiosity/src/config.py
@@ -3,11 +3,11 @@ class Config():
     def __init__(self):        
         self.gamma          = 0.99
 
-        self.eps_clip           = 0.2
+        self.eps_clip           = 0.1
         self.entropy_beta       = 0.01
 
-        self.buffer_size        = 2048
-        self.batch_size         = 64
+        self.buffer_size        = 1024
+        self.batch_size         = 32
         self.training_epochs    = 4 
 
         self.learning_rate  = 0.00025
