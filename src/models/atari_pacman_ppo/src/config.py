f06930d0dc25eeb6655013c2437d79c2df019bec
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-06-20 13:05:23 +0200

    results

--- src/models/atari_pacman_ppo/src/config.py
+++ src/models/atari_pacman_ppo/src/config.py
@@ -7,7 +7,7 @@ class Config():
         self.entropy_beta       = 0.01
 
         self.buffer_size        = 4096
-        self.batch_size         = 256
+        self.batch_size         = 64
         self.training_epochs    = 4 
 
         self.learning_rate  = 0.00025
