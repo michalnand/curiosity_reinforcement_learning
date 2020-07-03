a3f4cf2fdfeba051684c056903b8fb209cfca4a0
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-07-03 20:35:51 +0200

    atari dqn models

--- src/2_2048/models/dqn_b/src/config.py
+++ src/2_2048/models/dqn_b/src/config.py
@@ -5,13 +5,13 @@ class Config():
     def __init__(self):
         self.gamma = 0.99
 
-        self.update_frequency = 4
+        self.update_frequency = 16
 
         self.batch_size     = 64 
         self.learning_rate  = 0.0001
 
         self.exploration = common.decay.Exponential(0.999999, 1.0, 0.02, 0.02)
 
-        self.experience_replay_size = 8192
+        self.experience_replay_size = 16384
  
 
--- src/2_2048/models/dqn_curiosity/src/config.py
+++ src/2_2048/models/dqn_curiosity/src/config.py
@@ -5,14 +5,14 @@ class Config():
     def __init__(self):
         self.gamma = 0.99
 
-        self.update_frequency = 4
+        self.update_frequency = 16
 
         self.batch_size     = 64 
         self.learning_rate  = 0.0001
 
         self.exploration = common.decay.Exponential(0.999999, 1.0, 0.02, 0.02)
 
-        self.experience_replay_size = 8192
+        self.experience_replay_size = 16384
 
 
         self.curiosity_update_steps     = 8
